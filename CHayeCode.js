b2bced19201ff3e7b4e64cf12da4c2550203f675
Shifa Rabbani <shifarabbani@gmail.com>
2025-11-15 06:01:21 -0800

    first commit

---
+++ CHayeCode.js
@@ -0,0 +1,15 @@
+
+// https://github.com/lionheartAbd/https-github.com-hiteshchoudhary-js-hindi-youtube
+
+// name= "Shifa"
+// age=21
+// console.log(`my name is ${name} and my age is ${age}`);
+
+
+const str= new String("Hello Wolrd")
+console.log(str);
+console.log(str.charCodeAt(3));//give us a unicode value
+console.log("Shifa".concat (" Rabbnai"));
+console.log(str.endsWith('y'));
+
+ 
