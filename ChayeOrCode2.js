c194f08a56f13e0efe9095393a9fd53085be6353
Shifa Rabbani <shifarabbani@gmail.com>
2025-12-10 05:38:53 -0800

    Add new files

--- ChayeOrCode2.js
+++ ChayeOrCode2.js
@@ -1,19 +1,21 @@
 //   *************************Array**************
-let arr=["SHifa Rabbani",89,2,12,]
+// let arr=["SHifa Rabbani",89,2,12,]
 
- arr.push("Gundapor")
-arr.pop()
-arr.shift()
-arr.unshift("hy")
-console.log( arr.includes("by"));//give result in a bolean T/F
-console.log(arr.indexOf("SHifa Rabbani"));
-const  newArr=arr.join()//Convert arry into string 
-console.log( newArr);
-console.log(arr);
+//  arr.push("Gundapor")
+// arr.pop()
+// arr.shift()
+// arr.unshift("hy")
+// console.log( arr.includes("by"));//give result in a bolean T/F
+// console.log(arr.indexOf("SHifa Rabbani"));
+// const  newArr=arr.join()//Convert arry into string 
+// console.log( newArr);
+// console.log(arr);
 
-console.log(arr.slice(1,3));
-console.log(arr);
+// console.log(arr.slice(1,3));
+// console.log(arr);
 
-console.log(arr.splice(1,2));
-console.log( arr);
+// console.log(arr.splice(1,2));
+// console.log( arr);
 
+// **************************Real Time Problem******************
+// **************************Step1**************************
